eb9be97137b91531e551f5158b45b2a0173f285c
Spencer337 <spencer.m.walter@gmail.com>
2026-03-03 17:52:55 -0500

    Backup dusting mechanic is now working, as the player clicks, the layers of the fossil turn invisible

--- Assets/Spencer/Scripts/DustOnClick.cs
+++ Assets/Spencer/Scripts/DustOnClick.cs
@@ -5,11 +5,16 @@ public class DustOnClick : MonoBehaviour
 {
     public Vector3 positionOnFossil;
     public LayerMask sandLayerMask;
-    public Texture2D fossilTexture;
+    //public Texture2D fossilTexture;
+    public Material topLayer, middleLayer, bottomLayer;
+    public float opacity, currentlayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentlayer = 0;
+        opacity = 1;
+        topLayer.color = new Color(1f, 1f, 1f, 1f);
+        middleLayer.color = new Color(1f, 1f, 1f, 1f);
     }
 
     // Update is called once per frame
@@ -26,8 +31,33 @@ public class DustOnClick : MonoBehaviour
 
             if (Physics.Raycast(mouseClickRay, out mouseClickHit, 25f, sandLayerMask))
             {
-                positionOnFossil = mouseClickHit.point;
-                Debug.Log(positionOnFossil);
+                Debug.Log("dusting");
+                if (currentlayer == 0)
+                {
+                    opacity -= 0.1f;
+                    topLayer.color = new Color(1f, 1f, 1f, opacity);
+                    if (opacity <= 0)
+                    {
+                        currentlayer++;
+                        opacity = 1;
+                    }
+                }
+                else if (currentlayer == 1)
+                {
+                    opacity -= 0.1f;
+                    middleLayer.color = new Color(1f, 1f, 1f, opacity);
+                    if (opacity <= 0)
+                    {
+                        currentlayer++;
+                        opacity = 1;
+                    }
+                }
+                else
+                {
+
+                }
+                //positionOnFossil = mouseClickHit.point;
+                //Debug.Log(positionOnFossil);
             }
         }
 

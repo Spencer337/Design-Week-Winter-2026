d91afd41109c8ca7bcbadffa1655a323db587ef1
Tyr Rotsaert <tyrcat2005@gmail.com>
2026-03-03 17:11:45 -0500

    Scene Transition Almost Done
    finished scene transition minus making the UI only appear in range of the digsite.

--- Assets/Tyr/Scripts/Scr_Digsite.cs
+++ Assets/Tyr/Scripts/Scr_Digsite.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class Scr_Digsite : MonoBehaviour
 {
     public BoxCollider trigger;
-    public GameObject player; 
+    public GameObject player;
+    public bool playerInTrigger;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,6 +20,25 @@ public class Scr_Digsite : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(trigger.bounds.Contains(player.transform.position))
+        {
+            playerInTrigger = true;
+        }
+        else
+        {
+            playerInTrigger = false;
+        }
+
+        if (Keyboard.current != null && Keyboard.current.eKey.wasPressedThisFrame && playerInTrigger == true)
+        {
+            sceneSwapper();
+        }
+
     }
+    public void sceneSwapper()
+    {
+        SceneManager.LoadScene(sceneName: "Dusting Test");
+    }
+
 }
+
